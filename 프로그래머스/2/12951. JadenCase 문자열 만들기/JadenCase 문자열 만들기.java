a43130d3cdc3d55ba12a55ad66202c3f7d5d0581
minsungwhan <148740938+minsungwhan@users.noreply.github.com>
2023-11-06 19:04:22 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,31 +1,61 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s) {
-        char[] arr = s.toCharArray();
-	  boolean isSpace = true;
-	  StringBuilder sb = new StringBuilder();
-	  for(char c : arr) {
-	  if(c == ' ') {
-	  isSpace = true;
-	  sb.append(' ');
-	  } else {
-	  if(isSpace) {
-	  if(c >= 'a' && c <= 'z') {
-	  sb.append((char)(c - 'a' + 'A'));
-	  } else {
-	  sb.append(c);
-	  }
-	  isSpace = false;
-	  } else {
-	  if(c >= 'A' && c <= 'Z') {
-	  sb.append((char)(c + 'a' - 'A'));
-	  } else {
-	  sb.append(c);
-	  }
-	  }
-	  }
-	  }
-	  return sb.toString();
-    }
+		char[] arr = s.toCharArray();     //공백도 넣어준다.
+		boolean isSpace = true; // 첫번째문자이면 true, 아니면 false
+		StringBuilder sb = new StringBuilder();
+		for (char c : arr) {
+			if (c == ' ') { // 공백인 경우
+				isSpace = true; // 공백다음 문자는 첫번째문자니까 true
+				sb.append(' ');
+			} else {
+				if (isSpace) { // 첫번째 문자인 경우
+					if (c >= 'a' && c <= 'z') { // 소문자인 경우
+						sb.append((char) (c - 'a' + 'A'));
+					} else {
+						sb.append(c);
+					}
+					isSpace = false;
+				} else { // 첫번째 문자가 아닌 경우
+					if (c >= 'A' && c <= 'Z') { // 대문자인 경우
+						sb.append((char) (c + 'a' - 'A'));
+					} else {
+						sb.append(c);
+					}
+				}
+			}
+		}
+		return sb.toString();
+	}
 }
+
+
+
+
+
+      // 태완센세
+	  // public static String solution(String s) {
+	  // char[] arr = s.toCharArray();
+	  // boolean isSpace = true;
+	  // StringBuilder sb = new StringBuilder();
+	  // for(char c : arr) {
+	  // if(c == ' ') {
+	  // isSpace = true;
+	  // sb.append(' ');
+	  // } else {
+	  // if(isSpace) {
+	  // if(c >= 'a' && c <= 'z') {
+	  // sb.append((char)(c - 'a' + 'A'));
+	  // } else {
+	  // sb.append(c);
+	  // }
+	  // isSpace = false;
+	  // } else {
+	  // if(c >= 'A' && c <= 'Z') {
+	  // sb.append((char)(c + 'a' - 'A'));
+	  // } else {
+	  // sb.append(c);
+	  // }
+	  // }
+	  // }
+	  // }
+	  // return sb.toString();
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 81.1 MB, 시간: 0.05 ms
+메모리: 74.6 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 18:55:52
+2023년 11월 1일 19:4:21
 
 ### 문제 설명
 
